95c9364b1fcd0ee6dd28efc181dd34e56d5f1e5b
GeneCPChung <genecpc@gmail.com>
2018-02-06 20:47:54 -0500

    add fol

---
+++ 1.2/js/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+$(function() {
+
+    //settings for slider
+    var width = 270;
+
+      if ($(window).width() > 760) {
+            width = 720;
+        } 
+
+    var animationSpeed = 2000;
+    var pause =7000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+});
